ba5a74f093f93304f29790157538cfbe0f03c8f8
Andreas Madsack <andreas@madflex.de>
2022-03-06 16:15:04 +0100

    generalize comparision script and add majority vote columns

--- compare_tags.py
+++ compare_tags.py
@@ -1,4 +1,5 @@
 import csv
+import math
 from collections import defaultdict
 from pathlib import Path
 
@@ -7,20 +8,25 @@ import click
 from utils import yaml
 
 
+def calc_votes(labellers, row, vote):
+    return len([row[i] for i in labellers if row[i] == vote])
+
+
 @click.command()
 @click.option(
     "--folder", type=click.Path(exists=True), help="folder with tags-<name>.yml files"
 )
-def main(folder):
+@click.option("--tagname", default="blurred")
+def main(folder, tagname):
     data = defaultdict(dict)
     labellers = []
-    for fn in Path(folder).glob("*.yml"):
+    for fn in Path(folder).glob("*-*.yml"):
         # assumes tags-<name>.yml files, i.e. tags-mfa.yml
         labeller_id = fn.name.split("-")[-1].split(".")[0]
         labellers.append(labeller_id)
         for image_name, tags in yaml.load(open(fn)).items():
             for tag, value in tags.items():
-                if tag == "blurred":
+                if tag == tagname:
                     data[image_name][labeller_id] = {tag: value}
     test_files = [
         "G0010979.JPG",
@@ -46,19 +52,31 @@ def main(folder):
     ]
 
     with open(Path(folder) / "comparision.csv", "w", newline="") as csvfile:
-        fieldnames = ["image_name", "test"] + [i for i in labellers] + ["agreement"]
+        fieldnames = (
+            ["image_name", "test"]
+            + [i for i in sorted(labellers)]
+            + ["agreement", "majority_value", "majority"]
+        )
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
         for image_name, d in sorted(data.items()):
+            # only use images all labellers decided for
             if d.keys() == set(labellers):
                 row = {"image_name": image_name, "test": image_name in test_files}
                 for k, v in d.items():
                     for tag, value in v.items():
-                        # NOTE: this works only for one tag
                         row[k] = tag if value else f"not_{tag}"
 
-                row["agreement"] = len(set([row[i] for i in d.keys()])) == 1
+                row["agreement"] = len(set([row[i] for i in labellers])) == 1
+
+                pos_votes = calc_votes(labellers, row, tagname)
+                neg_votes = calc_votes(labellers, row, f"not_{tagname}")
+                half = math.ceil(len(labellers) / 2)
+                row["majority"] = (pos_votes > half) or (neg_votes > half)
+                row["majority_value"] = (
+                    tagname if pos_votes > neg_votes else f"not_{tagname}"
+                )
 
                 writer.writerow(row)
 
