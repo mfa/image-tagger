97b1a9bc6c90a7539655ed7c5577feb2580f5b76
Andreas Madsack <andreas@madflex.de>
2022-03-24 13:53:49 +0100

    add parameter for tagname

--- copy_filtered.py
+++ copy_filtered.py
@@ -12,21 +12,19 @@ from utils import yaml
 @click.option(
     "--destination", type=click.Path(exists=True), help="copy images to this folder"
 )
-def main(images, data, destination):
+@click.option("--tagname", help="tag to filter by")
+def main(images, data, destination, tagname):
     fn = Path(data) / "tags.yml"
     if not fn.exists():
         click.echo(f"tags.yml not found in {data}")
 
     dest = Path(destination)
-    (dest / "blured").mkdir(exist_ok=True)
-    (dest / "not_blured").mkdir(exist_ok=True)
+    (dest / tagname).mkdir(exist_ok=True)
+    (dest / f"not_{tagname}").mkdir(exist_ok=True)
 
     for image_name, tags in yaml.load(open(fn)).items():
         tags = dict(tags)
-        if tags.get("ignore"):
-            continue
-
-        folder = "blured" if tags.get("blured") else "not_blured"
+        folder = tagname if tags.get(tagname) else f"not_{tagname}"
         print(f"{image_name} to {folder}")
         shutil.copy(Path(images) / image_name, dest / folder)
 
